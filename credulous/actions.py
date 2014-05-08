a75c99e1817905aec7119b46a647b9de2fe95c15
Stephen Moore <stephen@delfick.com>
2014-05-08 23:56:22 +1000

    Whitespace, sigh

--- credulous/actions.py
+++ credulous/actions.py
@@ -1,15 +1,15 @@
 import os
 
 def do_display(credulous, **kwargs):
-	"""Just print out the chosen creds"""
-	access_key, secret_key = credulous.current_creds
-	print "export AWS_ACCESS_KEY_ID='{0}'".format(access_key)
-	print "export AWS_SECRET_ACCESS_KEY='{0}'".format(secret_key)
+    """Just print out the chosen creds"""
+    access_key, secret_key = credulous.current_creds
+    print "export AWS_ACCESS_KEY_ID='{0}'".format(access_key)
+    print "export AWS_SECRET_ACCESS_KEY='{0}'".format(secret_key)
 
 def do_exec(credulous, command, **kwargs):
-	"""Exec some command with aws credentials in the environment"""
-	access_key, secret_key = credulous.current_creds
-	environment = dict(os.environ)
-	environment.update(AWS_ACCESS_KEY_ID=access_key, AWS_SECRET_ACCESS_KEY=secret_key)
-	os.execvpe(command[0], command, environment)
+    """Exec some command with aws credentials in the environment"""
+    access_key, secret_key = credulous.current_creds
+    environment = dict(os.environ)
+    environment.update(AWS_ACCESS_KEY_ID=access_key, AWS_SECRET_ACCESS_KEY=secret_key)
+    os.execvpe(command[0], command, environment)
 
