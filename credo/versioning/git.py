327134bf773ed70a9857ef83968a77178e56c960
Stephen Moore <stephen@delfick.com>
2014-06-09 23:56:53 +1000

    Adding initial commit when creating the repo

--- credo/versioning/git.py
+++ credo/versioning/git.py
@@ -41,8 +41,44 @@ class GitDriver(Base):
 
         self.repo.create_remote("origin", new_remote)
 
+    def get_git_user(self):
+        """Get us a user for our git"""
+        name = self.repo.config.get_multivar("user.name")
+        if name:
+            name = name[0]
+        else:
+            name = "Credo"
+
+        email = self.repo.config.get_multivar("user.email")
+        if email:
+            email = email[0]
+        else:
+            email = "credo@nowhereinparticular.com"
+
+        return pygit2.Signature(name, email)
+
+    def current_commit(self):
+        """Return the current commit"""
+        head = self.repo.lookup_reference("HEAD")
+        try:
+            return [head.resolve().target]
+        except KeyError:
+            return []
+
     def add_change(self, message, changed_files):
-        pass
+        """Add a change to the repo"""
+        if not changed_files:
+            log.warning("Told to add a change, but no files were specified\tmessage=%s", message)
+        elif changed_files is True:
+            self.repo.index.add_all([])
+        else:
+            for filename in changed_files:
+                self.repo.index.add(filename)
+
+        self.repo.index.write()
+        oid = self.repo.index.write_tree()
+        author = committer = self.get_git_user()
+        self.repo.create_commit('HEAD', author, committer, message, oid, self.current_commit())
 
     def is_versioned(self):
         """Yes we are versioned"""
@@ -68,6 +104,7 @@ class GitDriver(Base):
             raise GitError("Trying to initialise git, but .git folder already exists", location=self.location)
 
         pygit2.init_repository(self.location)
+        self.add_change("Initial commit", changed_files=True)
         if new_remote:
             self.change_remote(new_remote)
 
